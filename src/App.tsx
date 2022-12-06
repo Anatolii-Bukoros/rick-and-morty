c2d60db028f315d78ffe5c46ba749096795f36d9
Anatolii Bukoros <bukoros.anatoliy@gmail.com>
2022-12-06 14:59:16 +0200

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.css';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 export const App: React.FC<{}> = () => {
   return (
     <div className="App">
+      <nav className="navbar">
+        <Link to='/' >Home</Link>
+        <Link to='characters' >characters</Link>
+        <Link to='locations' >locations</Link>
+        <Link to='episodes' >episodes</Link>
+      </nav>
+
       <Outlet />
     </div>
   )
