0f03d6dd6def83254a3575541d0c9e6a2a055b63
Anatolii Bukoros <bukoros.anatoliy@gmail.com>
2022-12-06 15:30:10 +0200

    test endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export const App: React.FC<{}> = () => {
   return (
     <div className="App">
       <nav className="navbar">
-        <Link to='/' >Home</Link>
+        <Link to='/rick-and-morty/' >Home</Link>
         <Link to='characters' >characters</Link>
         <Link to='locations' >locations</Link>
         <Link to='episodes' >episodes</Link>
