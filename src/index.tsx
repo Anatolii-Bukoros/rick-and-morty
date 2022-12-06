97081a394236d3e66d623ebc2948be73a237ea7d
Anatolii Bukoros <bukoros.anatoliy@gmail.com>
2022-12-06 14:35:32 +0200

    fixed home page

--- src/index.tsx
+++ src/index.tsx
@@ -13,15 +13,17 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="characters" element={<Characters />}>
-          <Route index element={<Characters />} />
-          <Route path=":charactersId" element={<Characters />} />
+        <Route path="/" element={<App />}>
+          <Route path="characters" element={<Characters />}>
+            <Route index element={<Characters />} />
+            <Route path=":charactersId" element={<Characters />} />
+          </Route>
+          <Route path="locations" element={<h1>Locations</h1>} />
+          <Route path="episodes" element={<h1>Episodes</h1>} />
+          <Route index element={<h1>Home</h1>} />
+          <Route path='home' element={<Navigate to="/" replace />} />
+          <Route path="*" element={<h1>Not Found Page</h1>} />
         </Route>
-        <Route path="locations" element={<h1>Locations</h1>} />
-        <Route path="episodes" element={<h1>Episodes</h1>} />
-        <Route index element={<Navigate to="/" replace />} />
-        <Route path="*" element={<h1>Not Found Page</h1>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
